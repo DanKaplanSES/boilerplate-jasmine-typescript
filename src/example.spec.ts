0c0ce62bb5b3091e09e3be47c3df0cf5b53526c0
Daniel Kaplan <daniel.kaplan@SleepEasySoftware.com>
2023-11-20 13:26:00 -0800

    WIP: should probably implement heap increase/decrease/change key for improvement
    That way I can use a heap instead of a collectionMap

--- src/example.spec.ts
+++ src/example.spec.ts
@@ -1,18 +1,99 @@
 import 'jasmine';
-
-import { example } from './example';
+import { System } from './example';
 
 describe('Example', () => {
-  
-  it('passes', () => {
-    example();
-    expect(true).toBeFalsy();
+  it('Acceptance Test', () => {
+    const system = new System();
+    system.add(`file1.txt`, 100);
+    system.add(`file2.txt`, 200, 'collection1');
+    system.add(`file3.txt`, 200, 'collection1');
+    system.add('file4.txt', 300, 'collection2');
+    system.add('file5.txt', 100);
+
+    expect(system.totalSize()).toEqual(900);
+    const collections = system.sortedCollections();
+
+    expect(collections[0]).toEqual({ collection: 'collection1', size: 400 });
+    expect(collections[1]).toEqual({ collection: 'collection2', size: 300 });
   });
 
+  it(`totalSize()`, () => {
+    const system = new System();
+
+    system.add(`file1.txt`, 100);
+    expect(system.totalSize()).toEqual(100);
+
+    system.add(`file2.txt`, 200, 'collection1');
+    expect(system.totalSize()).toEqual(300);
+
+    system.add(`file3.txt`, 200, 'collection1');
+    expect(system.totalSize()).toEqual(500);
+
+    system.add('file4.txt', 300, 'collection2');
+    expect(system.totalSize()).toEqual(800);
+
+    system.add('file5.txt', 100);
+    expect(system.totalSize()).toEqual(900);
+  });
+
+  it(`sortedCollections()`, () => {
+    const system = new System();
+
+    system.add(`file1.txt`, 100);
+    expect(system.sortedCollections()).toEqual(
+      jasmine.objectContaining({
+        length: 0,
+      })
+    );
+
+    system.add(`file2.txt`, 200, 'collection1');
+    expect(system.sortedCollections()).toEqual(
+      jasmine.objectContaining({
+        length: 1,
+        0: { collection: 'collection1', size: 200 },
+      })
+    );
+
+    system.add(`file3.txt`, 200, 'collection1');
+    expect(system.sortedCollections()).toEqual(
+      jasmine.objectContaining({
+        length: 1,
+        0: { collection: 'collection1', size: 400 },
+      })
+    );
+
+    system.add('file4.txt', 300, 'collection2');
+    expect(system.sortedCollections()).toEqual(
+      jasmine.objectContaining({
+        length: 2,
+        0: { collection: 'collection1', size: 400 },
+        1: { collection: 'collection2', size: 300 },
+      })
+    );
+
+    system.add('file5.txt', 100);
+    expect(system.sortedCollections()).toEqual(
+      jasmine.objectContaining({
+        length: 2,
+        0: { collection: 'collection1', size: 400 },
+        1: { collection: 'collection2', size: 300 },
+      })
+    );
+
+    system.add('file6.txt', 500, 'collection2');
+    expect(system.sortedCollections()).toEqual(
+      jasmine.objectContaining({
+        length: 2,
+        0: { collection: 'collection2', size: 800 },
+        1: { collection: 'collection1', size: 400 },
+      })
+    );
+  });
 });
 
 /*
-Given a list of [FileName, FileSize, [Collection]] - Collection is optional, i.e., a collection can have 1 or more files. Same file can be a part of more than 1 collection.
+Given a list of [FileName, FileSize, [Collection]] - Collection is optional, i.e., a collection can have 1 or more files. 
+Same file can be a part of more than 1 collection.
 How would you design a system
 
 To calculate total size of files processed.
--- src/example.ts
+++ src/example.ts
@@ -1,3 +1,39 @@
-export function example() {
-  
+type CollectionSize = { collection: string; size: number };
+
+export class System {
+  files: { name: string; size: number; collections: string[] }[] = [];
+  collectionMap: Map<string, number> = new Map<string, number>();
+
+  add(name: string, size: number, ...collections: string[]) {
+    this.files.push({ name, size, collections });
+    collections.forEach((collectionName) => {
+      if (!this.collectionMap.has(collectionName)) {
+        this.collectionMap.set(collectionName, 0);
+      }
+      const currentCollectionSize = this.collectionMap.get(collectionName);
+      if (currentCollectionSize !== undefined) {
+        this.collectionMap.set(collectionName, currentCollectionSize + size);
+      } else {
+        throw new Error(
+          `this.collectionMap.get(${collectionName}) === undefined`
+        );
+      }
+    });
+  }
+
+  totalSize(): number {
+    return this.files.reduce((prev, curr) => {
+      return prev + curr.size;
+    }, 0);
+  }
+
+  sortedCollections(): CollectionSize[] {
+    return [...this.collectionMap.entries()]
+      .map((entry): CollectionSize => {
+        return { collection: entry[0], size: entry[1] };
+      })
+      .sort((first: CollectionSize, second: CollectionSize) => {
+        return second.size - first.size;
+      });
+  }
 }
